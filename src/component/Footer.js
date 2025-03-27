deaa3634ea97c63320938c8bdb39dbd170161f57
bugloss12 <ataraxia@kau.kr>
2025-03-27 21:39:06 +0900

    third commit

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,19 +3,61 @@ import "../styles/Footer.css";
 
 const Footer = () => {
     return(
-        <div>
+        <div className="footer-container">
             <div className="footer-section">
                 <div className="footer-title">
                     LIKELION PROCESS
                 </div>
-            <div className="footer-subtitle">
+                <div className="footer-subtitle">
                 멋쟁이사자처럼은 창업 동아리이므로 실제 상품을 판매하지 않으며 연출된 페이지입니다.
+                </div>
             </div>
-        </div>
-        <div className="info-text-wrapper">
-        <div className="info-text-1">상호명</div>
-        <div className="info-text-2">멋쟁이사자처럼</div>
-        </div>
+            <div className="footer-section">
+                <div className="info-text-row">
+                    <div className="info-text-wrapper">
+                        <div className="info-text-1">상호명</div>
+                        <div className="info-text-2">멋쟁이사자처럼</div>
+                    </div>
+                    <div className="info-text-wrapper">
+                        <div className="info-text-1">대표</div>
+                        <div className="info-text-2">룬</div>
+                    </div>
+                    <div className="info-text-wrapper">
+                        <div className="info-text-1">사업자등록번호</div>
+                        <div className="info-text-2">123-4567-890</div>
+                    </div>
+                    <div className="info-text-wrapper">
+                        <div className="info-text-1">주소</div>
+                        <div className="info-text-2">경기도 고양시 덕양구 한국항공대학교</div>
+                    </div>
+                    <div className="info-text-wrapper">
+                        <div className="info-text-1">상호명</div>
+                        <div className="info-text-2">멋쟁이사자처럼</div>
+                    </div>
+            </div>
+                <div className="info-text-row">
+                    <div className="info-text-wrapper">
+                            <div className="info-text-1">상호명</div>
+                        <div className="info-text-2">멋쟁이사자처럼</div>
+                    </div>
+                    <div className="info-text-wrapper">
+                        <div className="info-text-1">대표</div>
+                        <div className="info-text-2">룬</div>
+                    </div>
+                    <div className="info-text-wrapper">
+                        <div className="info-text-1">사업자등록번호</div>
+                        <div className="info-text-2">123-4567-890</div>
+                    </div>
+                    <div className="info-text-wrapper">
+                        <div className="info-text-1">주소</div>
+                        <div className="info-text-2">경기도 고양시 덕양구 항공대학로</div>
+                    </div>
+                    <div className="info-text-wrapper">
+                        <div className="info-text-1">상호명</div>
+                        <div className="info-text-2">멋쟁이사자처럼</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
